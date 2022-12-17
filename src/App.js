da103c5d82006a96adc1e0438833d8dd2a40bcbe
AshadulTopu <ashadulislam2003@gmail.com>
2022-12-18 03:16:32 +0600

    Create a simple react app using API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import './tailwind/style.css';
-import  SideBar from './components/Header/SideBar'
-// import FullContent from './components/FullContent/FullContent'
-// import HttpRequestDemo from './components/FullContent/test';
+
+import  SideBar from './components/Header/SideBar';
 function App() {
   return (
     <div className="App w-4/5 m-auto">
     
-     
-     {/* <h1 className='text-red-500'>Hello Ashadul</h1> */}
-      
       <SideBar />
-      {/* <FullContent /> */}
-      {/* <HttpRequestDemo /> */}
-      
       
     </div>
   );
--- src/components/Header/SideBar.js
+++ src/components/Header/SideBar.js
@@ -2,14 +2,20 @@
 import { useEffect,useState } from "react"
 import axios from "axios"
 
-export default function SideBar (){
+export default function SideBar(){
     const [surahs,setSurahs] = useState([]);
     const [audioFile, setAudioFile] = useState(null);
-    const [select, setSelect] = useState(null);
+    const [selectedSurah, setSelectedSurah] = useState(null);
+    const [content, setContent] = useState([]);
+    const [selectedContent, setSelectedContent] = useState(null);
 
     const changeSelectedSurah = (id) => {
-        setSelect(id)
+        setSelectedSurah(id)
+        setSelectedContent(id)
     }
+    // const changeSelectedContent = (id) => {
+    //     setSelectedContent(id)
+    // }
 
 
     useEffect(() => {
@@ -21,13 +27,24 @@ export default function SideBar (){
 
 
     useEffect(() => {
-        if(select) {
-            axios.get(`https://api.quran.com/api/v4/chapter_recitations/2/${select}`)
+        if(selectedSurah) {
+            axios.get(`https://api.quran.com/api/v4/chapter_recitations/2/${selectedSurah}`)
             .then(data => {
+                // console.log(data);
                 setAudioFile(data.data.audio_file)
             })
         }
-    }, [select])
+    },[selectedSurah])
+
+    useEffect(() => {
+        if(selectedContent){
+            axios.get(`https://api.quran.com/api/v4/verses/by_chapter/${selectedContent}?language=en&words=false&translations=161&audio=7&tafsirs=true&fields=text_uthmani,verse_number,image_url&page=1&per_page=400`)
+            .then(response =>{
+                console.log(response);
+                setContent(response.data.verses)
+            })
+        }
+    },[selectedContent])
 
 
     return (
@@ -37,20 +54,30 @@ export default function SideBar (){
                     {
                         surahs.map((chapter)=>(
 
-                        <li key={chapter.id} onClick={() => changeSelectedSurah(surahs.id)} className=" p-2  shadow-md shadow-white-500 text-white hover:bg-white hover:text-black hover:rounded hover:shadow-2xl transition duration-500   ">
-                        <a className="font-sans text-xl "  href="">{chapter.id} {chapter.name_simple} - <small> {chapter.revelation_place} </small></a></li>
+                    <li key={chapter.id} onClick={() => [changeSelectedSurah(chapter.id),/*changeSelectedContent(chapter.id)*/]} className=" p-2  shadow-md shadow-white-500 text-white hover:bg-white hover:text-black hover:rounded hover:shadow-2xl cursor-pointer transition duration-500   ">
+                        <h4 className="font-sans text-xl ">{chapter.id} - {chapter.name_simple} - <small> {chapter.revelation_place} </small></h4></li>
                         ))
                     }
                 </ul>
             </div>
             <div className="w-4/5 ">
-            {/* <audio src="" controls></audio>
-             */}
+                <div className="w-full h-40 p-10">
                 {
                     audioFile?.audio_url && (
-                        <audio src={`${audioFile.audio_url}`} controls />
+                        <audio src={`${audioFile.audio_url}`} className="fixed" controls />
                     )
                 }
+                </div>
+                <div className="p-10">
+                {
+                    content.map(info=>(
+                    <div>
+                        <p className="text-right text-white leading-10 p-2">{info.text_uthmani}</p>
+                        <p className=" text-white p-2 leading-10"> <span className="w-6 h-5 p-2  bg-cyan-400 rounded-full ">{info.verse_number}</span>{info.translations[0].text}</p>
+                    </div>
+                    ))
+                }
+                </div>
             </div>
         </div>
     )}
